bb73ed9c5b4ed39e815c225d2b179c4719c4e3f6
Gautam3994 <gautam3994@gmail.com>
2020-02-25 20:00:12 +0530

    commit

--- VPC/vpc/lab-setup.ps1
+++ VPC/vpc/lab-setup.ps1
@@ -1,8 +1,8 @@
 # Lab-Setup.ps1
 
 #Replace the values below and save this file as credentials.ps1
-$AWSAccessKey="AKIAJ2HCVRW3WZ6SEGHQ" # Replace with your access key
-$AWSSecretKey="9s5kkQ89QibR5k0aHS62wltJhCL1ldnkidnDjms+" # Replace with your secret key
+$AWSAccessKey="" # Replace with your access key
+$AWSSecretKey="" # Replace with your secret key
 
 # Set AWS credentials and region
 $AWSProfileName="aws-networking-deep-dive-vpc"
